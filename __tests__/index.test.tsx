14bd04c10dbdd9361e43ec056697e60b057887af
EzequielPalma <ezequielpalmat@gmail.com>
2024-01-15 10:39:00 +0100

    hhh

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -12,6 +12,13 @@ describe('Home', () => {
       name: /welcome to next\.js!/i,
     })
 
-    expect(heading).toBeInTheDocument()
+    // Modifica el selector para buscar un elemento <h2>
+    const h2Heading = screen.getByRole('heading', {
+      name: /your custom h2 text/i,
+      level: 2,
+    })
+
+    // Asegúrate de que el elemento <h2> esté en el documento
+    expect(h2Heading).toBeInTheDocument()
   })
 })
